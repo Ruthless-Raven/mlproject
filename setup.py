66cf260a30676aed6c5d46646b856769892bb466
isaac kim <isaac.kim@ecstech.com>
2024-02-13 14:30:44 -1000

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+name = 'mlproject',
+version = '0.0.1',
+author = 'Isaac'
+author_email = 'ihkim2@outlook.com'
+packages = find_packages()
+install_requires = ['pandas', 'numpy', 'seaborn']
+
+
+
+
+)
